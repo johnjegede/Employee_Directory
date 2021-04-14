0c8545cec51e4a0577bfa910821ea66db9b1435c
John Jegede <jegede400@gmail.com>
2021-04-14 11:08:02 -0400

    display of employee

--- reactapp/src/components/EmployeeContainer.js
+++ reactapp/src/components/EmployeeContainer.js
@@ -6,18 +6,57 @@ import API from "../utils/API";
 class EmployeeContainer extends Component {
   state = {
     search: "",
-    results: [],
+    employees: [],
   };
 
   componentDidMount() {
-    this.searchEmployee();
+    this.getEmployee();
   }
 
-  searchEmployee = () => {
-      API.search()
-         .then(res => this.setState({ results: res.data.data}))
-         .catch(err => console.log(err));
+  getEmployee = () => {
+    API.get()
+      .then((res) => this.setState({ employees: res.data.data }))
+      .catch((err) => console.log(err));
   };
 
+  handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
+    if (value.length > 0) {
+      let filterEmployees = this.state.employees.filter((employee) => {
+        if (
+          employee.name.first.toLowerCase().includes(value) ||
+          employee.name.last.toLowerCase().includes(value) ||
+          employee.email.toLowerCase().includes(value) ||
+          employee.phone.toLowerCase().includes(value) ||
+          employee.dob.date.toLowerCase().includes(value)
+        ) {
+          return employee;
+        } else {
+          return false;
+        }
+      });
+      this.setState({ ...this.state, employees: filterEmployees });
+    } else {
+      //this.setState({...this.state, employees: this.state.originalList});
+    }
+    console.log(this.state.employees);
+  };
+
+  render() {
+    return (
+      <div>
+        <EmployeeForm
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+        />
+        <EmployeeList results={this.state.employees} />
+      </div>
+    );
+  }
 }
+
 export default EmployeeContainer;
--- reactapp/src/components/EmployeeList.js
+++ reactapp/src/components/EmployeeList.js
@@ -2,13 +2,29 @@ import React from "react";
 
 function EmployeeList(props) {
   return (
-    <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
-    
-        </li>
-      ))}
-    </ul>
+    <table className="table text-center">
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col" >Name</th>
+          <th scope="col" className="email">Email</th>
+          <th scope="col" className="city">location</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+          {props.employees.map(result=>( 
+          <tr key={result.id.value}>
+            <td><img src={result.picture.thumbnail} alt=""/> </td>
+            <td>{result.name.first} {result.name.last}</td>
+            <td className="email">{result.email}</td>
+            <td className="city">{result.location.city}</td>
+            
+        </tr>
+          ))}
+        
+      </tbody>
+    </table>
   );
 }
 
